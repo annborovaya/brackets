28fe0a465489feacd89ff9e3405bb9e2947f9955
Anna Borovaya <any.georg@gmail.com>
2023-10-04 00:22:21 +0200

    feat: add func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const openBrackets = [];
+  const closeBrackets = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openBrackets.push(bracketsConfig[i][0]);
+    closeBrackets.push(bracketsConfig[i][1]);
+  }
+
+  let stack = [];
+
+  function checkBrackets(str) {
+
+    for (let i = 0; i < str.length; i++) {
+      let currentBracket = str[i];
+
+      if (openBrackets.includes(currentBracket)) {
+        stack.push(currentBracket);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+      }
+
+      let lastElement = stack[stack.length - 1];
+      let equalOpenBracket = openBrackets[closeBrackets.indexOf(currentBracket)];
+
+      if (lastElement === equalOpenBracket) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
